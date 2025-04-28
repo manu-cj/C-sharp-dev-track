7298fa89d631ec097ebc79d63645e787cc4e09c1
manu-cj <carenjotje@gmail.com>
2025-04-28 10:23:43 +0200

    add exercice

---
+++ 10-csharp-methods-manu-cj/Solution/Methods/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Methods
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            
+        }
+    }
+}
---
+++ 10-csharp-methods-manu-cj/Solution/Methods/Solution.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Methods
+{
+    public class Solution
+    {
+      public static int Sum(int firstNumber, int secondNumber)
+       {
+          return firstNumber + secondNumber;
+       }
+
+       public static string Whos(string firstname, string lastname, int age)
+       {
+        return $"Firstname : {firstname}\nLastname : {lastname}\nAge : {age}";
+       }
+
+      public static string SumAndProduct(int firstNumber, int secondNumber, out int sum, out int product)
+      {
+          sum = firstNumber + secondNumber;
+          product = firstNumber * secondNumber;
+          return $"Sum: {sum}, Product: {product}";
+      }
+
+      public static (int quotient, int remainder) QuotientAndRemainder(int firstNumber, int secondNumber) 
+      {
+        return (
+          firstNumber / secondNumber,
+          firstNumber % secondNumber
+        );
+      }
+
+      public static int MethodWithDefaultValue(int value = 10)
+      {
+        return value * 2; 
+      }
+
+
+    }
+}
